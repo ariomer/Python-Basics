7121d4631250fda5d3d2cc94de435fa73cbbb436
Omer Ari <66365119+ariomer@users.noreply.github.com>
2020-06-06 21:23:18 +0900

    Add files via upload

---
+++ multiply_05062020.py
@@ -0,0 +1,10 @@
+def carp():
+    print('Ilk sayiyi giriniz:')
+    num1 = int(input())
+    print('Ikinci sayiyi giriniz:')
+    num2 = int(input())
+    print('Iki sayinin carpim sonucu:')
+    print(num1 * num2)
+    
+    
+carp()
