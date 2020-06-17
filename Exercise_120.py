71c1f3b20168dee571c98714cd6b77c827bdeeaa
Omer Ari <66365119+ariomer@users.noreply.github.com>
2020-06-17 15:22:54 +0900

    Add files via upload

---
+++ Exercise_120.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 17 15:20:35 2020
+
+@author: ts-omer.ari
+"""
+
+
+# =============================================================================
+# Python Basic: Exercise-120 with Solution
+# Write a Python program to format a specified string to limit the number of characters to 6.
+# =============================================================================
+
+str_num = "1234567890"
+print()
+print('%.6s' % str_num)
+print()
